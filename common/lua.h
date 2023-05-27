63f4691d81ea172566a192591e939512276c5f9e
guanguowei <1326317931@qq.com>
2023-05-27 10:29:28 +0800

    2023.5.27

--- common/lua.h
+++ common/lua.h
@@ -11,4 +11,15 @@ static int POINTER_SIZE = sizeof(void*);
 #define LUA_NUMBER float
 #endif
 
+// basic object type
+#define LUA_TNUMBER 1
+#define LUA_TLIGHTUSERDATA 2
+#define LUA_TBOOLEAN 3
+#define LUA_TSTRING 4
+#define LUA_TNIL 5
+#define LUA_TTABLE 6
+#define LUA_TFUNCTION 7
+#define LUA_TTHREAD 8
+#define LUA_TNONE 9
+
 #endif
--- common/luaobject.h
+++ common/luaobject.h
@@ -24,5 +24,34 @@ typedef struct lua_TValue {
 	int tt_;
 } TValue;
 
+// lua number type 
+#define LUA_NUMINT (LUA_TNUMBER | (0 << 4))	// 十进制数值：1
+#define LUA_NUMFLT (LUA_TNUMBER | (1 << 4))	// 十进制数值：17
+
+// lua function type 
+#define LUA_TLCL (LUA_TFUNCTION | (0 << 4))	// 十进制数值：7
+#define LUA_TLCF (LUA_TFUNCTION | (1 << 4)) // 十进制数值：23
+#define LUA_TCCL (LUA_TFUNCTION | (2 << 4)) // 十进制数值：39
+
+// string type 
+#define LUA_LNGSTR (LUA_TSTRING | (0 << 4))	// 十进制数值：4
+#define LUA_SHRSTR (LUA_TSTRING | (1 << 4))	// 十进制数值：20
 
 #endif
+
+/**
+
+
+
+LUA_NUMINT 		二进制：0000 0001	十进制：1
+LUA_NUMFLT 		二进制：0001 0001	十进制：17
+
+LUA_TLCL 		二进制：0000 0111	十进制：7
+LUA_TLCF 		二进制：0001 0111	十进制：23
+LUA_TCCL 		二进制：0010 0111	十进制：39
+
+LUA_LNGSTR 		二进制：0000 0100	十进制：4
+LUA_SHRSTR 		二进制：0001 0100	十进制：20
+
+ * 
+//
