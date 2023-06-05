72de5acc795944ea954c218bb90d2eb7d4e6e7d0
guanguowei <1326317931@qq.com>
2023-06-05 21:05:31 +0800

    2023.6.5

--- vm/luado.c
+++ vm/luado.c
@@ -1,15 +1,36 @@
 #include "luado.h"
 
 int luaD_calll(struct lua_State* L, StkId func, int nresult) {
-    if (++(L->ncalls) > LUA_MAXCALLS) {
-        luaD_throw(L, 0);
-    }
+	if (++(L->ncalls) > LUA_MAXCALLS) { // lua_State 调用数量检查
+		luaD_throw(L, 0);
+	}
 
-    if (!luaD_precall(L, func, nresult)) {
+	if (!luaD_precall(L, func, nresult)) {
 
-    }
+	}
 
-    (L->ncalls)--;
-    return LUA_OK;
+	(L->ncalls)--;
+	return LUA_OK;
 }
 
+
+int luaD_precall(struct lua_State* L, StkId func, int nresult) {
+	switch (func->tt_) {
+		case LUA_TLCF:{
+			lua_CFunction f = func->value_.f;
+
+			ptrdiff_t func_diff = savestack(L, func);
+			luaD_checkstack(L, LUA_MINSTACK);
+			func = restorestack(L, func_diff);
+
+			next_ci(L, func, nresult);
+			int n = (*f)(L);
+			assert(L->ci->func + n <= L->ci->top);
+			return 1;
+		}
+			break;
+		default:
+			break;
+	}
+	return 0;
+}
--- vm/luado.h
+++ vm/luado.h
@@ -5,4 +5,6 @@
 
 int luaD_call(struct lua_State* L, StkId func, int nresult);
 
+int luaD_precall(struct lua_State* L, StkId func, int nresult);
+
 #endif
